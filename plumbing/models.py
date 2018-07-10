45294c34532372948f63cec34dcea7edc1d35184
VLaD0S <tudorvladneacsu@hotmail.co.uk>
2018-07-10 11:17:28 +0100

    minor change in models

--- plumbing/models.py
+++ plumbing/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 from django import forms
-from django.conf import settings
 
 # Create your models here.
 class review(models.Model):
